03f88d941a33e707a19f20f1fa87ca87999d604c
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-11-05 08:42:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, func){
+    let empty = []
+    for (let i = 0; i  < array.length; i++){
+    let el = array[i]
+    empty.push(func(el))
+    }
+    return empty;
+}
+
+function reduce(array, func, startingPoint=0){
+    let empty = []
+    for (let i=0;  i < array.length; i++){
+    let el = array[i]
+    empty.push(func(el))
+    }
+    return empty;
+}
